28dcdd80cd5c452a808de71fe10b049c53b13d83
Nate <150736820+NateHerrera@users.noreply.github.com>
2026-04-20 20:09:28 -0500

    Write to an output file

--- PasswordCombos/combo.py
+++ PasswordCombos/combo.py
@@ -2,9 +2,10 @@
 # i.e. 00aa, 00ab, 00ac, ..., 99zy, 99zz
 import string
 
-for i in range(100):
-    padded_string = str(i)
-    padded_numbers = padded_string.zfill(2)
-    for letter1 in string.ascii_lowercase:
-        for letter2 in string.ascii_lowercase:
-            print(padded_numbers + letter1 + letter2)
+with open("output.txt", "w") as file:
+    for i in range(100):
+        padded_string = str(i)
+        padded_numbers = padded_string.zfill(2)
+        for letter1 in string.ascii_lowercase:
+            for letter2 in string.ascii_lowercase:
+                file.write(padded_numbers + letter1 + letter2 + "\n")
